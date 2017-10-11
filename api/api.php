c9d84f6d1f8f0abbc07f99ab2a3abc9866b77bdd
Larbonard <leonard.labes@gmx.de>
2017-10-11 17:00:25 +0200

    Get single JsonMap by Id

--- api/api.php
+++ api/api.php
@@ -10,6 +10,7 @@ require('../databasecon.php');
 session_start();
 $username = $_SESSION["Username"]?? "";
 if(isset($_GET['maplist'])) getMapList($con,"");
+else if(isset($_GET['jsonmap'])) getJsonMap($con,$_GET['mapid'],"");
 
 function getMapList($con,$username)
 {
@@ -23,6 +24,24 @@ function getMapList($con,$username)
             'favorite' => false
         );
     }
+    $jsonFile = json_encode($map);
+    echo $jsonFile;
+}
+function getJsonMap($con,$mapId,$username)
+{
+    $sql=$con->prepare('SELECT MapId,Name,JsonMap FROM Map WHERE MapId = ?');
+    $sql->execute(array($mapId));
+    $row= $sql->fetch();
+    if($row)
+    {
+        $map = array(
+            'id' => $row['MapId'],
+            'name' => $row['Name'],
+            'map' => json_decode($row ['JsonMap'], true),
+            'permission' => 0,
+            'favorite' => false
+        );
+    }else $map = array('error' => 'Error: No map with this id found.');
     $jsonFile = json_encode($map);
     echo $jsonFile;
 }
